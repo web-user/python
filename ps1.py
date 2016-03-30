1f85018434a056ac31b5121f723c0309b06262f8
web_user <srounov@gmail.com>
2016-03-30 12:16:08 +0000

    for counter

--- ps1.py
+++ ps1.py
@@ -1,10 +1,32 @@
-def reverse_name(first_name,last_name):
-    if first_name and last_name:
-        reversed_name = ' '.join([last_name, first_name])
-    else:
-        reversed_name = 'Error'
-    return reversed_name
+# def reverse_name(first_name,last_name):
+#     if first_name and last_name:
+#         reversed_name = ' '.join([last_name, first_name])
+#     else:
+#         reversed_name = 'Error'
+#     return reversed_name
 
-first_name = raw_input('What is first name? ')
-last_name = raw_input('What is last namee? ')
-print(reverse_name(first_name, last_name))
+# first_name = raw_input('What is first name? ')
+# last_name = raw_input('What is last namee? ')
+# print(reverse_name(first_name, last_name))
+# python stop 
+# import sys
+# sys.exit("Error message")
+
+# a = [1, 4, 5, 7, 8, 0, 23]
+
+# currenr = None
+
+# while currenr != 0:
+#     currenr = a.pop(0)
+#     print(currenr)
+# print('End')
+
+def find(string, substring):
+    counter = 0
+    for start in range(len(string) - len(substring)):
+        if string[start: start + len(substring)] == substring:
+            counter += 1
+    return counter
+
+print(find('azcbobobegghakl', 'bob'))
+
